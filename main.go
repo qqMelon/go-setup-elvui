f9a9798bc8aa5816af1353d8d05d37de505862a3
qqMelon <urie.zahard@gmail.com>
2022-08-22 09:21:48 +0200

    Restruc archi

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-setup-elvui/utils"
 	"os"
 	"reflect"
 	"time"
@@ -11,15 +12,9 @@ import (
 
 func fileReader(filename string) string {
 	data, err := os.ReadFile(filename)
-	errorHandler(err)
+	utils.ErrorHandler(err)
 
 	return string(data)
-}
-
-func errorHandler(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
 
 func getElvuiVersion() string {
